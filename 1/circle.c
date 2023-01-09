861fce2dc5029bf80ccf3cda1d4904cb9b94a2b9
Rishad Hossain <rishadmahmud11@gmail.com>
2023-01-10 01:05:13 +0600

    10-1-23

--- 1/circle.c
+++ 1/circle.c
@@ -9,6 +9,6 @@ void main()
     scanf("%f",&r);
     d = 2*r ;
     c = 2*PI*r ;
-    a = PI*pow(r,2);
+    a = PI*r*r;
     printf("The Diameter of this circle is\t%.3f\nThe circumference of this circle is\t%.3f\nThe Area of this circle is\t%.3f\n",d,c,a);
 }
