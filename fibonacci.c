b84c54e63ba1eb976632b6219e8e1d01e3bfa913
Rishad Hossain <rishadmahmud11@gmail.com>
2023-02-13 12:10:51 +0600

    13-02-ver

--- fibonacci.c
+++ fibonacci.c
@@ -5,7 +5,7 @@ void main()
     int t3,t1=0,t2=1,i,n;
     printf("The Program will print the fibonacci series till your given number.\nPlease enter the number :");
     scanf("%d",&n);
-    for(i=3,t3=0;t3<=n;i++)
+    for(i=3,t3=0;i<=n;i++)
     {
         printf("%d\n",t1);
         t3=t1+t2;
